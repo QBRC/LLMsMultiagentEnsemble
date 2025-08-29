b9cc29e01f599a3c7db7def719131c2dbd510c65
Jingwei Huang <jingwei.huang@utsouthwestern.edu>
2025-08-29 02:43:02 -0500

    remove unused

--- tmp/.ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-for p in ['ensemble_llms_app','llm_app','llm_agent','fix_json','evaluate']:
-    package_dir = os.path.abspath(f"../{p}")
-    if package_dir not in sys.path:
-        sys.path.insert(0, package_dir)
